993656cd644e8027ac910f061cf2b3241099d8be
Harsh <deadspaceharsh@gmail.com>
2019-04-18 03:12:51 +0530

    add values

--- main.js
+++ main.js
@@ -1,2 +1,14 @@
-var app_name = angular.module()
+var app_name = angular.module('app_name', []);
+
+app_name.controller('ctrl1', function($scope){
+
+    $scope.first = 1;
+    $scope.second = 1;
+
+    $scope.updateValue = function () {
+         $scope.calculation = $scope.first + ' + ' + $scope.second + ' = ' + 
+            (+$scope.first + +$scope.second);
+    }
+
+});
 
